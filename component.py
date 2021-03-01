da914f1d0c10c3e6bc5b8fd218d7e67c5246daff
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-01 15:05:44 +0900

    研究室の紹介機関外のときの対応を追加

--- component.py
+++ component.py
@@ -19,29 +19,33 @@ import pytz
 from io import StringIO
 
 from linebot.models import *
-#lineが登録されたときに行われるイベント
-def create_new(usrid,username):
-    #lineのユーザー情報を登録する
+
+
+# lineが登録されたときに行われるイベント
+def create_new(usrid, username):
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
 
-    #ユーザーIDがある場合は登録を拒否
+    # ユーザーIDがある場合は登録を拒否
     if usrid in list(df["ユーザーID"]):
-        dupiricate_message = "すでに{}様は澤田研究室の見学名簿に登録されています。\n確認から名簿があるか確認してみてください。".format(username)
+        dupiricate_message = "すでに{}様は澤田研究室の見学名簿に登録されています。\n確認から名簿があるか確認してみてください。".format(
+            username)
         return dupiricate_message
 
-    #ユーザーIDがない場合には登録を許可
+    # ユーザーIDがない場合には登録を許可
     elif usrid not in list(df["ユーザーID"]):
-        #取得したユーザー情報を元にcsvを更新
+        # 取得したユーザー情報を元にcsvを更新
         s3_resource = boto3.resource('s3')
-        new_dt = pd.DataFrame([[usrid,dt_now.strftime('%Y/%m/%d'),dt_now.strftime('%H:%M'), username,"",""]], columns=col)
+        new_dt = pd.DataFrame([[usrid, dt_now.strftime(
+            '%Y/%m/%d'), dt_now.strftime('%H:%M'), username, "", ""]], columns=col)
         df2 = df.append(new_dt, ignore_index=True)
 
         s3_object = s3_resource.Object(BUCKET_NAME, KEY)
-        s3_object.put(Body=df2.to_csv(None,index=False).encode('utf_8_sig'))
+        s3_object.put(Body=df2.to_csv(None, index=False).encode('utf_8_sig'))
 
         reg_text = '''登録ありがとうございました
 これより研究室の見学の登録などが行なえます。
@@ -49,114 +53,119 @@ def create_new(usrid,username):
         return reg_text
 
 
-#日付を獲得する処理
-def get_date(date_str,username):
-    #lineのユーザー情報を登録する
+# 日付を獲得する処理
+def get_date(date_str, username):
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #名前が登録されているユーザーの場合
+    # 名前が登録されているユーザーの場合
     try:
-        #ユーザーが居るとき
+        # ユーザーが居るとき
         if username in list(df["名前"]):
-            #usernameを含まない行の取り出し
-            df_ori = df[df["名前"]!=username]
-            #usernameを含む行の取り出し→処理ではこっちをつかう
-            df_user = df[df["名前"]==username]
+            # usernameを含まない行の取り出し
+            df_ori = df[df["名前"] != username]
+            # usernameを含む行の取り出し→処理ではこっちをつかう
+            df_user = df[df["名前"] == username]
 
-            #日程希望を登録
+            # 日程希望を登録
             df_user.希望日時 = date_str
 
-            #データを再合成
-            df2 = pd.concat([df_ori,df_user])
+            # データを再合成
+            df2 = pd.concat([df_ori, df_user])
 
-            #取得した体重を元にcsvを更新
+            # 取得した体重を元にcsvを更新
             s3_resource = boto3.resource('s3')
 
             s3_object = s3_resource.Object(BUCKET_NAME, KEY)
-            s3_object.put(Body=df2.to_csv(None,index=False).encode('utf_8_sig'))
+            s3_object.put(Body=df2.to_csv(
+                None, index=False).encode('utf_8_sig'))
 
-            #終了を知らせるメッセージ
-            complete_text = "{}様の日程を{}で承りました".format(username,date_str)
+            # 終了を知らせるメッセージ
+            complete_text = "{}様の日程を{}で承りました".format(username, date_str)
             return complete_text
-        #ユーザーがいないとき
+        # ユーザーがいないとき
         else:
             fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
             return fail_text
 
-    #usernameがなかったときの処理
+    # usernameがなかったときの処理
     except:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
 
-#メール・アドレスを取得した際の処理
-def get_mail(mail_text,username):
-    #lineのユーザー情報を登録する
+# メール・アドレスを取得した際の処理
+
+
+def get_mail(mail_text, username):
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #名前が登録されているユーザーの場合
+    # 名前が登録されているユーザーの場合
     try:
         if username in list(df["名前"]):
-            #usernameを含まない行の取り出し
-            df_ori = df[df["名前"]!=username]
-            #usernameを含む行の取り出し→処理ではこっちをつかう
-            df_user = df[df["名前"]==username]
+            # usernameを含まない行の取り出し
+            df_ori = df[df["名前"] != username]
+            # usernameを含む行の取り出し→処理ではこっちをつかう
+            df_user = df[df["名前"] == username]
 
-            #日程希望を登録
+            # 日程希望を登録
             df_user.メールアドレス = mail_text
 
-            #データを再合成
-            df2 = pd.concat([df_ori,df_user])
+            # データを再合成
+            df2 = pd.concat([df_ori, df_user])
 
-            #取得したメールを元にcsvを更新
+            # 取得したメールを元にcsvを更新
             s3_resource = boto3.resource('s3')
 
             s3_object = s3_resource.Object(BUCKET_NAME, KEY)
-            s3_object.put(Body=df2.to_csv(None,index=False).encode('utf_8_sig'))
+            s3_object.put(Body=df2.to_csv(
+                None, index=False).encode('utf_8_sig'))
 
-            #終了を知らせるメッセージ
-            complete_text = "{}様のメールアドレスを{}で承りました".format(username,mail_text)
+            # 終了を知らせるメッセージ
+            complete_text = "{}様のメールアドレスを{}で承りました".format(username, mail_text)
             return complete_text
         else:
             fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
             return fail_text
 
-    #usernameがなかったときの処理
+    # usernameがなかったときの処理
     except:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
 
-#ユーザー情報の確認
+# ユーザー情報の確認
+
+
 def show_user(username):
-    #lineのユーザー情報を登録する
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #ユーザーネームがあることが最低条件
+    # ユーザーネームがあることが最低条件
     if username in list(df["名前"]):
-        df_user = df[df["名前"]==username]
+        df_user = df[df["名前"] == username]
         df_user = df_user.reset_index()
-        shere='''
+        shere = '''
 名前:{}
 希望日時:{}
 メールアドレス:{}
 
 で登録いただいております。
 訂正したい場合、再度日程またはメールアドレスから上書きを行ってください。
-'''.format(df_user["名前"][0],df_user["希望日時"][0].replace("T"," "),df_user["メールアドレス"][0])
+'''.format(df_user["名前"][0], df_user["希望日時"][0].replace("T", " "), df_user["メールアドレス"][0])
         return shere
-    #ユーザーネームがない場合
+    # ユーザーネームがない場合
     else:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
-
 
 
 BUCKET_NAME = "pypy-test"
@@ -181,26 +190,26 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-#lineが登録されたときに行われるイベント
+# lineが登録されたときに行われるイベント
 
 
 def create_new(usrid, username):
-    #lineのユーザー情報を登録する
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
 
-    #ユーザーIDがある場合は登録を拒否
+    # ユーザーIDがある場合は登録を拒否
     if usrid in list(df["ユーザーID"]):
         dupiricate_message = "すでに{}様は澤田研究室の見学名簿に登録されています。\n確認から名簿があるか確認してみてください。".format(
             username)
         return dupiricate_message
 
-    #ユーザーIDがない場合には登録を許可
+    # ユーザーIDがない場合には登録を許可
     elif usrid not in list(df["ユーザーID"]):
-        #取得したユーザー情報を元にcsvを更新
+        # 取得したユーザー情報を元にcsvを更新
         s3_resource = boto3.resource('s3')
         new_dt = pd.DataFrame([[usrid, dt_now.strftime(
             '%Y/%m/%d'), dt_now.strftime('%H:%M'), username, "", ""]], columns=col)
@@ -215,103 +224,103 @@ def create_new(usrid, username):
         return reg_text
 
 
-#日付を獲得する処理
+# 日付を獲得する処理
 def get_date(date_str, username):
-    #lineのユーザー情報を登録する
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #名前が登録されているユーザーの場合
+    # 名前が登録されているユーザーの場合
     try:
-        #ユーザーが居るとき
+        # ユーザーが居るとき
         if username in list(df["名前"]):
-            #usernameを含まない行の取り出し
+            # usernameを含まない行の取り出し
             df_ori = df[df["名前"] != username]
-            #usernameを含む行の取り出し→処理ではこっちをつかう
+            # usernameを含む行の取り出し→処理ではこっちをつかう
             df_user = df[df["名前"] == username]
 
-            #日程希望を登録
+            # 日程希望を登録
             df_user.希望日時 = date_str
 
-            #データを再合成
+            # データを再合成
             df2 = pd.concat([df_ori, df_user])
 
-            #取得した体重を元にcsvを更新
+            # 取得した体重を元にcsvを更新
             s3_resource = boto3.resource('s3')
 
             s3_object = s3_resource.Object(BUCKET_NAME, KEY)
             s3_object.put(Body=df2.to_csv(
                 None, index=False).encode('utf_8_sig'))
 
-            #終了を知らせるメッセージ
+            # 終了を知らせるメッセージ
             complete_text = "{}様の日程を{}で承りました".format(username, date_str)
             return complete_text
-        #ユーザーがいないとき
+        # ユーザーがいないとき
         else:
             fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
             return fail_text
 
-    #usernameがなかったときの処理
+    # usernameがなかったときの処理
     except:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
 
-#メール・アドレスを取得した際の処理
+# メール・アドレスを取得した際の処理
 
 
 def get_mail(mail_text, username):
-    #lineのユーザー情報を登録する
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #名前が登録されているユーザーの場合
+    # 名前が登録されているユーザーの場合
     try:
         if username in list(df["名前"]):
-            #usernameを含まない行の取り出し
+            # usernameを含まない行の取り出し
             df_ori = df[df["名前"] != username]
-            #usernameを含む行の取り出し→処理ではこっちをつかう
+            # usernameを含む行の取り出し→処理ではこっちをつかう
             df_user = df[df["名前"] == username]
 
-            #日程希望を登録
+            # 日程希望を登録
             df_user.メールアドレス = mail_text
 
-            #データを再合成
+            # データを再合成
             df2 = pd.concat([df_ori, df_user])
 
-            #取得したメールを元にcsvを更新
+            # 取得したメールを元にcsvを更新
             s3_resource = boto3.resource('s3')
 
             s3_object = s3_resource.Object(BUCKET_NAME, KEY)
             s3_object.put(Body=df2.to_csv(
                 None, index=False).encode('utf_8_sig'))
 
-            #終了を知らせるメッセージ
+            # 終了を知らせるメッセージ
             complete_text = "{}様のメールアドレスを{}で承りました".format(username, mail_text)
             return complete_text
         else:
             fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
             return fail_text
 
-    #usernameがなかったときの処理
+    # usernameがなかったときの処理
     except:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
 
-#ユーザー情報の確認
+# ユーザー情報の確認
 
 
 def show_user(username):
-    #lineのユーザー情報を登録する
+    # lineのユーザー情報を登録する
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
     body = obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    #ユーザーネームがあることが最低条件
+    # ユーザーネームがあることが最低条件
     if username in list(df["名前"]):
         df_user = df[df["名前"] == username]
         df_user = df_user.reset_index()
@@ -324,7 +333,8 @@ def show_user(username):
 訂正したい場合、再度日程またはメールアドレスから上書きを行ってください。
 '''.format(df_user["名前"][0], df_user["希望日時"][0].replace("T", " "), df_user["メールアドレス"][0])
         return shere
-    #ユーザーネームがない場合
+    # ユーザーネームがない場合
     else:
         fail_text = "{}様は澤田研究室に登録されていません\n登録し直してください".format(username)
         return fail_text
+
--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,8 @@
 from component import *
 
+# 期間内, 期間外を設定する変数
+inner_date = False
+
 BUCKET_NAME = "pypy-test"
 KEY = "kenken.csv"
 dt_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
@@ -10,6 +13,7 @@ logger.setLevel(logging.ERROR)
 col = ["ユーザーID", '登録日', '時間', '名前', '希望日時', 'メールアドレス']
 
 
+# lineのアクセスシークレットとトークンが設定されているか確認
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
@@ -19,16 +23,22 @@ if channel_access_token is None:
     logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
+
+# アクセストークンとアクセスシークレットをセット
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
 # lambdaの処理に入る
 def lambda_handler(event, context):
+
+    # X-Line-Signatureからx-line-signatureに変更になったのでチェック
     if "x-line-signature" in event["headers"]:
         signature = event["headers"]["x-line-signature"]
     elif "X-Line-Signature" in event["headers"]:
         signature = event["headers"]["X-Line-Signature"]
+
+    # 認証できなかったときのjsonを返す
     body = event["body"]
     ok_json = {"isBase64Encoded": False,
                "statusCode": 200,
@@ -39,102 +49,104 @@ def lambda_handler(event, context):
                   "headers": {},
                   "body": "Error"}
 
-    # #フォローされたときのイベント
-    # @handler.add(FollowEvent)
-    # def handle_follow(event):
-    #     with open('./date.json') as f:
-    #         date_message = json.load(f)
-    #     line_bot_api.reply_message(
-    #         event.reply_token,
-    #         #alt_textがないとエラーになるので注意
-    #         FlexSendMessage(alt_text='訪問日', contents=date_message)
-    #     )
+    if inner_date == True:
+        @handler.add(MessageEvent, message=TextMessage)
+        def handle_text_message(event):
+            text = event.message.text
+            usrid = event.source.user_id
+            profile = line_bot_api.get_profile(event.source.user_id)
+            username = profile.display_name
+            if text == 'その他':
+                #             message=create_new(usrid,username)
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(
+                    text="わからないことがあれば\nreiwa2020@sawada.phys.waseda.ac.jp\nまで連絡ください"))
+            elif text == "日程調整がしたい":
+                buttons_template = ButtonsTemplate(
+                    title='研究室見学について', text='研究室見学についての調整を行います', actions=[
+                        # startというデータのpostbackeventを発行
+                        PostbackAction(label='まずは登録', data='まずは登録'),
+                        PostbackAction(label='日程', data='日程'),
+                        PostbackAction(label='メールアドレス', data='メールアドレス'),
+                        PostbackAction(label='確認', data='確認'),
+                    ])
+                template_message = TemplateSendMessage(
+                    alt_text='Buttons alt text', template=buttons_template)
+                line_bot_api.reply_message(event.reply_token, template_message)
+            # メール・アドレスのときの分岐
+            elif "@" in text:
+                message = get_mail(text, username)
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=message))
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text='個別にメッセージにお答えすることができません。\n下のメニューより選択して下さい。'))
 
-    @handler.add(MessageEvent, message=TextMessage)
-    def handle_text_message(event):
-        text = event.message.text
-        usrid = event.source.user_id
-        profile = line_bot_api.get_profile(event.source.user_id)
-        username = profile.display_name
-        if text == 'その他':
+        # 日程調整の際のボタンの選択により分岐
+
+        @handler.add(PostbackEvent)
+        def handle_postback(event):
+            usrid = event.source.user_id
+            profile = line_bot_api.get_profile(event.source.user_id)
+            username = profile.display_name
+            data = event.postback.data
+
+            # startが押された際の登録 裏コマンド的な
+            if data == 'start':
+                buttons_template = ButtonsTemplate(
+                    title='My buttons sample', text='確認です。', actions=[
+                        # startというデータのpostbackeventを発行
+                        PostbackAction(label='start', data='start'),
+                        PostbackAction(label='end', data='end'),
+                        DatetimePickerAction(
+                            label='show', data='show', mode="datetime"),
+                        PostbackAction(label='del', data='del'),
+                    ])
+                template_message = TemplateSendMessage(
+                    alt_text='Buttons alt text', template=buttons_template)
+                line_bot_api.reply_message(event.reply_token, template_message)
+
+            # 日程調整が押されたときの分岐
+            elif data == '日程':
+                with open('./date.json') as f:
+                    date_message = json.load(f)
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    # alt_textがないとエラーになるので注意
+                    FlexSendMessage(alt_text='訪問日', contents=date_message)
+                )
+
+            # まずは登録を押された際の分岐
+            elif data == "まずは登録":
+                message = create_new(usrid, username)
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=message))
+
+            # 日程が押された際の分岐 dataに〜を含むのは日程のみ
+            elif "~" in data:
+                message = get_date(data, username)
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=message))
+
+            elif data == 'メールアドレス':
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text="メール・アドレスを登録してください"))
+
+            elif data == '確認':
+                message = show_user(username)
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=message))
+                
+    # 期間外のときの処理
+    elif inner_date == False:
+        @handler.add(MessageEvent, message=TextMessage)
+        def handle_text_message(event):
             #             message=create_new(usrid,username)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(
-                text="わからないことがあれば\nreiwa2020@sawada.phys.waseda.ac.jp\nまで連絡ください"))
-        elif text == "日程調整がしたい":
-            buttons_template = ButtonsTemplate(
-                title='研究室見学について', text='研究室見学についての調整を行います', actions=[
-                    # startというデータのpostbackeventを発行
-                    PostbackAction(label='まずは登録', data='まずは登録'),
-                    PostbackAction(label='日程', data='日程'),
-                    PostbackAction(label='メールアドレス', data='メールアドレス'),
-                    PostbackAction(label='確認', data='確認'),
-                ])
-            template_message = TemplateSendMessage(
-                alt_text='Buttons alt text', template=buttons_template)
-            line_bot_api.reply_message(event.reply_token, template_message)
-        # メール・アドレスのときの分岐
-        elif "@" in text:
-            message = get_mail(text, username)
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text=message))
-        else:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='下のメニューから選択してください'))
-
-    # 日程調整の際のボタンの選択により分岐
-
-    @handler.add(PostbackEvent)
-    def handle_postback(event):
-        usrid = event.source.user_id
-        profile = line_bot_api.get_profile(event.source.user_id)
-        username = profile.display_name
-        data = event.postback.data
-
-        # startが押された際の登録 裏コマンド的な
-        if data == 'start':
-            buttons_template = ButtonsTemplate(
-                title='My buttons sample', text='確認です。', actions=[
-                    # startというデータのpostbackeventを発行
-                    PostbackAction(label='start', data='start'),
-                    PostbackAction(label='end', data='end'),
-                    DatetimePickerAction(
-                        label='show', data='show', mode="datetime"),
-                    PostbackAction(label='del', data='del'),
-                ])
-            template_message = TemplateSendMessage(
-                alt_text='Buttons alt text', template=buttons_template)
-            line_bot_api.reply_message(event.reply_token, template_message)
-
-        # 日程調整が押されたときの分岐
-        elif data == '日程':
-            with open('./date.json') as f:
-                date_message = json.load(f)
-            line_bot_api.reply_message(
-                event.reply_token,
-                # alt_textがないとエラーになるので注意
-                FlexSendMessage(alt_text='訪問日', contents=date_message)
-            )
-
-        # まずは登録を押された際の分岐
-        elif data == "まずは登録":
-            message = create_new(usrid, username)
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text=message))
-
-        # 日程が押された際の分岐 dataに〜を含むのは日程のみ
-        elif "~" in data:
-            message = get_date(data, username)
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text=message))
-
-        elif data == 'メールアドレス':
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text="メール・アドレスを登録してください"))
-
-        elif data == '確認':
-            message = show_user(username)
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text=message))
+                    text="只今研究室見学の対象期間ではありませんので予約をすることができません。(再開: 冬頃予定)"))
+        
+    
+    
+    # この記述がないとhandler is not　のエラーになる
     try:
         handler.handle(body, signature)
     except LineBotApiError as e:
